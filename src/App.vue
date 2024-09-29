16cbe5757a0808083205793e8c6d4760dd99abe5
xiaomabenteng <xiaomabenteng@look.com>
2024-09-29 23:39:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,43 @@
 <script setup lang="ts">
-import { createAppKit, useAppKit } from '@reown/appkit/vue'
+import { createAppKit, useAppKit, useAppKitProvider, useAppKitAccount  } from '@reown/appkit/vue'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
 import { useWriteContract } from '@wagmi/vue'
 import { abi } from './abi'
+import { BrowserProvider, Contract, ethers, formatUnits } from 'ethers'
 
 const { writeContract } = useWriteContract()
+
+const USDTAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+
+  // The ERC-20 Contract ABI, which is a common contract interface
+  // for tokens (this is the Human-Readable ABI format)
+  const USDTAbi = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+    'function balanceOf(address) view returns (uint)',
+    'function transfer(address to, uint amount)',
+    'event Transfer(address indexed from, address indexed to, uint amount)',
+    'function approve(spender to, uint amount)'
+  ]
+
+  async function approve2() {
+    const { address, isConnected } = useAppKitAccount()
+    const { walletProvider } = useAppKitProvider('eip155')
+  try {
+    let window: any
+    const ethersProvider = new ethers.BrowserProvider(window.ethereum)
+    const signer = await ethersProvider.getSigner()
+    // The Contract object
+    const USDTContract = new Contract(USDTAddress, USDTAbi, signer)
+    const USDTBalance = await USDTContract.approve( '0x5ecA4288BFe530AB9b3cf455eE94c8951EA292bb', 
+    BigInt(100000000000) )
+    console.log('Approval successful');
+  } catch (error) {
+    console.error('Approval failed:', error);
+    alert('Approval failed: ' + error);
+  }
+}
 
 async function approve() {
   try {
@@ -56,7 +88,7 @@ const modal = useAppKit()
 
 <template>
   <w3m-button />
-  <button @click="approve">
+  <button @click="approve2">
     approve
   </button>
 </template>
