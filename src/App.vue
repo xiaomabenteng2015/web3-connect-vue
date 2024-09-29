e39109f3c20eb1c33b6d784f527838ebbb8b57ac
xiaomabenteng <xiaomabenteng@look.com>
2024-09-30 00:56:02 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ const USDTAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
     'function balanceOf(address) view returns (uint)',
     'function transfer(address to, uint amount)',
     'event Transfer(address indexed from, address indexed to, uint amount)',
-    'function approve(spender to, uint amount)'
+    'function approve(address spender, uint256 amount) returns (bool)'
   ]
 
   async function approve2() {
